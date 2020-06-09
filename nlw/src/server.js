550bc3088ff051accb9bc21fcda4680835139ab4
Carlos-Henrique98 <ichfs9108@gmail.com>
2020-06-09 20:39:50 -0300

    Ecoleta

---
+++ nlw/src/server.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const server = express()
+
+//configurar pasta publica
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views",{
+    express: server,
+    noCache: true
+})
+
+//configurar caminhos da minha aplicacao
+//página inicial 
+//req: requisicao
+//res: resposta
+server.get("/", (req,res) => {
+    return res.render("index.html",{title: "Um título"})
+})
+
+server.get("/create-point", (req, res) =>{
+    return res.render("create-point.html")
+})
+
+//ligar o servidor
+server.listen(3000)
